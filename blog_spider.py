644d1e3f2739c37080d0c57be08c628db7ca545f
Shenruixiang <diawavy212@gmail.com>
2026-04-19 21:35:37 +0800

    no.2

--- blog_spider.py
+++ blog_spider.py
@@ -1,12 +1,23 @@
 import requests
-
+from bs4 import BeautifulSoup
 urls=[
     f"https://www.cnblogs.com/#p{page}"
     for page in range(1,50+1)
 ]
-
+#生产者
 def craw(url):
     r=requests.get(url)
-    print(url,len(r.text))
+    #print(url,len(r.text))
+    return r.text
+
+def parse(html):
+    #class="post-item-title"
+    soup=BeautifulSoup(html,'html.parser')
+    links=soup.find_all('a',class_='post-item-title')
+    return [(link["href"],link.get_text()) for link in links]
 
 
+if __name__=="__main__":
+    #用for循环遍历解析结果里的每一个(href, title)，每次循环把结果赋值给result变量
+    for result in parse(craw(urls[2])):
+        print(result)
